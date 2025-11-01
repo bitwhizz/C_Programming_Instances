b05a62948df03d2db34f61ed08088c698a2aa922
bits2bytes-tech <saifullabaig55@gmail.com>
2025-11-01 22:32:09 +0530

    shuffles

--- print_float_2.c
+++ print_float_2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main() 
 { 
-		printf ("%-15.1f,hello\n", 12.3); 
+		printf ("%-15.1f,hello\n", 12.34); 
 		printf ("%-15.2f,hi\n", 123.);
 }
 /*observe there is a dot after 123. and it is float type so it is follow by zeros*/
