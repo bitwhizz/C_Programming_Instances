15e15eab22c8b66b3664a02e4e0373e875371561
bitwhizz <saifullabaig55@gmail.com>
2026-03-13 12:38:14 +0000

    editing variable names

--- jugglers/circular_rottation_of_array.c
+++ jugglers/circular_rottation_of_array.c
@@ -15,33 +15,50 @@ int main()
 {             
     int size;
     printf("Size of array: ");
-    scanf("%d",&size);    
+    scanf("%d",&size); 
+
     int arr[size];
     printf("Enter the elements ");
-    for(int a=0;a<size;a++)   
-    scanf("%d",&arr[a]);    
-    int n;
+    for(int i=0;i<size;i++)   
+        scanf("%d",&arr[i]); 
+
+    int rindex;
     printf("Enter the index from where you want your array to rotate ");
-    scanf("%d",&n);        
+    scanf("%d",&rindex);
+
     printf("Array: \n");    
-    for (int a = 0; a < size; a++) {     
-        printf("%d ", arr[a]);     
+    for (int i = 0; i < size; i++) {     
+        printf("%d ", arr[i]);     
     }             
 
-    for(int a = 0; a < n; a++) {
-        int b, temporary;
+    for(int i = 0; i < rindex; i++) {
+        int irotate, temporary;
         temporary = arr[size-1]; 
-        for(b = size-1; b > 0; b--)
+        for(irotate = size-1; irotate > 0; irotate--)
         {    
-            arr[b] = arr[b-1];    
+            arr[irotate] = arr[irotate-1];    
         }    
         arr[0] = temporary;    
     }            
+
     printf("\n");            
     printf("New Array: \n");    
     for(int a = 0; a< size; a++){    
         printf("%d ", arr[a]);    
     }    
-    printf("\n");         
+    printf("\n");    
+
     return 0;    
 }
+
+/*
+
+saif@saif-Inspiron-5547:~/programmer/C_Programming_Instances /jugglers$ ./circular_rottation_of_array 
+Size of array: 10
+Enter the elements 1 2 3 4 5 6 7 8 9 10
+Enter the index from where you want your array to rotate 6
+Array: 
+1 2 3 4 5 6 7 8 9 10 
+New Array: 
+5 6 7 8 9 10 1 2 3 4 
+*/
