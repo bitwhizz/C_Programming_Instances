cdc558d6f5bede35582e35a41c8f91567df20de9
bitwhizz <saifullabaig55@gmail.com>
2026-07-15 13:31:55 +0530

    corrections

--- operators/prece3.c
+++ operators/prece3.c
@@ -3,7 +3,7 @@ main()
 {
 	int a,b=5,c=11,d=10;
 	a= b++ & --c |  d++;
-	printf("a= %d\n b=%d\n c=%d\n d=%d\n",a,b,c);
+	printf("a= %d\n b=%d\n c=%d\n d=%d\n",a,b,c,d);
 	return 0;
 }
 /* && and || are the logical operators-always gives o/p in 1 or 0
